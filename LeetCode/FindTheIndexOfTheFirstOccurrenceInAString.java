63b8a1a1a812b5869b98afc08c8969b9fce1f318
Youssef Abdelaziz <yabdelaziz.123@gmail.com>
2023-05-15 13:49:26 +0300

    New Solutions on LeetCode

---
+++ LeetCode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public static int strStr(String haystack, String needle) {
+
+        if (haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        } else
+            return -1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("leetcode", "leeto"));
+    }
+}
